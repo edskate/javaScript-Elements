8cd21de0e5d79ca58014f4704abf8b1d11641b6e
Edilson Gomes de lima <edskateicevida@gmail.com>
2022-09-30 16:17:10 -0300

    foi adicionado um index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+    // Create element:
+    const para = document.createElement("p");
+    para.innerHTML = "This is a paragraph.";
+
+    // Append to another element:
+    document.getElementById("myDIV").appendChild(para);
+
+
+    document.querySelector("p").style.backgroundColor = "black";
+    document.querySelector("p").style.fontFamily = "sans";
+    document.querySelector("p").style.border = "4px solid ";
+document.querySelector("h1").style.height ="100px";
+    document.querySelector("p").style.color = "red";
+    document.querySelector("p").style.boxShadow = "12px 12px 13px black";
